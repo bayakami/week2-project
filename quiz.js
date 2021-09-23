ee67f68e9ced1e963bb4b9b0c3e5ad980e69f788
bayakami <90577236+bayakami@users.noreply.github.com>
2021-09-23 13:51:04 +0100

    questions kind of appear one a time now

---
+++ quiz.js
@@ -0,0 +1,41 @@
+/*hides welcome screen and displays question 1*/
+function startQuiz() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "block";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "none";
+    
+  }
+
+  /*hides question 1 and displays question 2*/
+  function question2() {
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "block";
+  }
+
+   /*hides question 2 and displays question 3*/
+   function question3() {
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "block";
+  }
+
+   /*hides question 3 and displays question 4*/
+   function question4() {
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "block";
+  }
+
+   /*hides question 4 and displays question 5*/
+   function question5() {
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "block";
+  }
+
+   /*hides question 5 and displays results
+   function question2() {
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "inline-block";
+  }*/
+
