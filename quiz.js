f0646c10f82d206b86bdcfa8cf62dd783cfa458e
bayakami <90577236+bayakami@users.noreply.github.com>
2021-09-24 10:19:59 +0100

    displays results, calcResults working (not correctly)

--- quiz.js
+++ quiz.js
@@ -2,11 +2,6 @@
 function startQuiz() {
     document.getElementById("intro").style.display = "none";
     document.getElementById("question1").style.display = "block";
-    document.getElementById("question2").style.display = "none";
-    document.getElementById("question3").style.display = "none";
-    document.getElementById("question4").style.display = "none";
-    document.getElementById("question5").style.display = "none";
-    
   }
 
   /*hides question 1 and displays question 2*/
@@ -33,9 +28,31 @@ function startQuiz() {
     document.getElementById("question5").style.display = "block";
   }
 
-   /*hides question 5 and displays results
-   function question2() {
-    document.getElementById("question1").style.display = "none";
-    document.getElementById("question2").style.display = "inline-block";
-  }*/
+   /*hides question 5 and displays results*/
+   function results() {
+    calcResults();
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("results").style.display = "block";
+  }
 
+
+function calcResults(){;
+    var score = 0;
+    var ele = document.getElementsByTagName("radio");
+    var result = document.getElementById("results");
+      for(i = 0; i < ele.length; i++) { 
+                if(ele[i].checked)             
+                score+=parseInt(ele[i].value)
+      }
+    
+
+  if (score<=8) {
+    message = "PERL <br>Your best days may be behind you, but you you've still got it. Once you make a friend they are friends for life.   You are unpredictable, but those who know you best are used to it."
+} else if (score>=9 && score<=12) {
+  message = "JAVA <br>You are complex yet thorough in your work. You know the solution to everyone's problem but your own."
+} else {
+  message = "PYTHON <br>You are educated and enjoy research, especially in science. You also love a good book when you have time to read. You pay attention to the details and you don't accept mistakes."
+}
+
+document.getElementById("score").innerHTML = message
+}
