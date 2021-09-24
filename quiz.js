2d11b94c0f18932db2e1b42a08023b3f17a54695
bayakami <90577236+bayakami@users.noreply.github.com>
2021-09-24 11:51:57 +0100

    calculates results now

--- quiz.js
+++ quiz.js
@@ -5,76 +5,56 @@ function startQuiz() {
   }
 
   /*hides question 1 and displays question 2*/
-  function question2() {
-    document.getElementById("question1").style.display = "none";
-    document.getElementById("question2").style.display = "block";
-  }
+function question2() {
+  document.getElementById("question1").style.display = "none";
+  document.getElementById("question2").style.display = "block";
+}
 
    /*hides question 2 and displays question 3*/
-   function question3() {
-    document.getElementById("question2").style.display = "none";
-    document.getElementById("question3").style.display = "block";
-  }
+function question3() {
+  document.getElementById("question2").style.display = "none";
+  document.getElementById("question3").style.display = "block";
+}
 
    /*hides question 3 and displays question 4*/
-   function question4() {
-    document.getElementById("question3").style.display = "none";
-    document.getElementById("question4").style.display = "block";
-  }
+function question4() {
+  document.getElementById("question3").style.display = "none";
+  document.getElementById("question4").style.display = "block";
+}
 
    /*hides question 4 and displays question 5*/
-   function question5() {
-    document.getElementById("question4").style.display = "none";
-    document.getElementById("question5").style.display = "block";
-  }
+function question5() {
+  document.getElementById("question4").style.display = "none";
+  document.getElementById("question5").style.display = "block";
+}
 
    /*hides question 5 and displays results*/
-   function results() {
-    calcResults();
-    document.getElementById("question5").style.display = "none";
-    document.getElementById("results").style.display = "block";
-  }
+function results() {
+  calcResults();
+  document.getElementById("question5").style.display = "none";
+  document.getElementById("results").style.display = "block";
+}
 
 /*score calculation*/
 function calcResults(){;
-    var score = 0;
-    var ele = document.getElementsByTagName("radio");
-    //var result = document.getElementById("results");
-      for(i = 0; i < ele.length; i++) { 
-                if(ele[i].checked)             
-                score+=parseInt(ele[i].value)
-      }
-    
-     /* switch(score) {
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-          message = "PERL";
-          break;
-        case 9:
-        case 10:
-        case 11:
-        case 12:
-          message = "JAVA";
-          break;
-        case 13:
-        case 14:
-        case 15:
-          message = "PYTHON"
-          break;
-       
-      }*/
-
+  var score = 0;
+  var ele = document.getElementsByTagName("input");
+  console.log(ele);
+  for(i = 0; i < ele.length; i++) {
+    if ((ele[i].type)=="radio") {
+      if(ele[i].checked)
+        score += parseInt(ele[i].value)            
+    } 
+  }
 
   if (score<=8) {
-    message = "PERL <br>Your best days may be behind you, but you you've still got it. Once you make a friend they are friends for life.   You are unpredictable, but those who know you best are used to it."
-} else if (score>=9 && score<=12) {
-  message = "JAVA <br>You are complex yet thorough in your work. You know the solution to everyone's problem but your own."
-} else {
-  message = "PYTHON <br>You are educated and enjoy research, especially in science. You also love a good book when you have time to read. You pay attention to the details and you don't accept mistakes."
-}
+    message = "PERL <br><br>Your best days may be behind you, but you you've still got it. Once you make a friend they are friends for life.   You are unpredictable, but those who know you best are used to it."
+  } else if (score>=9 && score<=12) {
+    message = "JAVA <br><br>You are complex yet thorough in your work. You know the solution to everyone's problem but your own."
+  } else {
+    message = "PYTHON <br><br>You are educated and enjoy research, especially in science. You also love a good book when you have time to read. You pay attention to the details and you don't accept mistakes."
+  }
 
-document.getElementById("score").innerHTML = message
-return score;
+
+  document.getElementById("score").innerHTML = message;
 }
